34b0065c99745e904f0fa56765f0bf9bbadb1535
William Gotterer <william.gotterer@gmail.com>
2021-10-12 09:51:44 -0400

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,16 @@ import Links from "./Links";
 
 function About(props) {
 
+  console.log(props)
+
   
   return (
     <div id="about">
       <h2>About Me</h2>
       {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <div>{props.github}{props.linkedin}</div>
+      <Links github={props.github} linkedin={props.linkedin} />
+      {/* <div>{props.github}{props.linkedin}</div> */}
       
     </div>
   );
--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-import Links from "./Links";
+
 
 // pass this data down as props to the child component(s) that need it!
 
@@ -13,10 +13,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home color={user.color} city={user.city} username={user.username}/>
-      <About bio={user.bio} github={user.github} linkedin={user.linkedin}/>
-      <Links />
-      
+      <Home color={user.color} city={user.city} username={user.name}/>
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin}/> 
       
      
     </div>
